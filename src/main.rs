0b75f650f20c306a3f4ea0d47990b309158cb76c
jlgoldb2 <jlgoldb2@asu.edu>
2015-01-10 12:02:37 -0700

    clock

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::time::duration::{Duration};
+use std::thread::Thread;
+
 fn main() {
-    println!("Hello, world!");
+  tick_per_second();
 }
+
+fn tick_per_second(){
+  let mut clock = Clock{interval: Duration::nanoseconds(1), count: 0};
+  clock.tick()
+}
+
+struct Clock {
+  interval: Duration,
+  count: u64,
+}
+
+impl Clock {
+  fn tick(&mut self){
+    loop {
+      self.count = self.count + 1;
+      println!("tick {}", self.count);
+      std::io::timer::sleep(self.interval);
+    }
+  }
+}
+
